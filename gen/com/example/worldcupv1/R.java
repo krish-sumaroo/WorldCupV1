e2bc4983f5e5e570751d2f9e22679a24892d75ee
romasoomaree <romasoomaree@gmail.com>
2014-02-21 22:14:45 +0400

    revert back the sample test commit

--- gen/com/example/worldcupv1/R.java
+++ gen/com/example/worldcupv1/R.java
@@ -21,7 +21,11 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int defaut_bg=0x7f020000;
+        public static final int flag_spain=0x7f020001;
+        public static final int flag_uruguay=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int splash=0x7f020004;
     }
     public static final class id {
         public static final int action_settings=0x7f080000;
@@ -36,7 +40,6 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
-        public static final int test=0x7f050003;
     }
     public static final class style {
         /** 

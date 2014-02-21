64a330ad4743068b8e2b3f84ad3130ba0b0c0bd0
romasoomaree <romasoomaree@gmail.com>
2014-02-21 22:18:00 +0400

    Add web service class

---
+++ src/com/competition/worldcupv1/webServicehelper/ServerHelper.java
@@ -0,0 +1,16 @@
+package com.competition.worldcupv1.webServicehelper;
+
+import com.competition.worldcupv1.databasehelper.DatabaseHelper;
+
+import android.content.Context;
+
+public class ServerHelper {
+	
+	private DatabaseHelper dbHelper;
+	private Context context;
+
+	public ServerHelper(Context context) {
+		this.context = context;
+	}
+
+}
